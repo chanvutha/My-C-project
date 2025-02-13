26cebb014a949597b4196037dcdac7fc1f88864c
chanvutha <106652271+chanvutha@users.noreply.github.com>
2025-02-13 17:46:54 +0700

    Create divider_calculator.h
    the header file

---
+++ divider_calculator.h
@@ -0,0 +1,10 @@
+#ifndef DIVIDER_CALCULATOR_H
+#define DIVIDER_CALCULATOR_H
+
+#include <stdio.h>
+
+// Function prototypes
+void voltageDivider(float Vin, float R1, float R2);
+void currentDivider(float Iin, float R1, float R2);
+
+#endif // DIVIDER_CALCULATOR_H
